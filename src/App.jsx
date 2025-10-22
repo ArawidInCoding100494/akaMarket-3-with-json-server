68481a3715793df53b9d23410183c2e38d96a137
Abdurashid Abdumajidov <abdumajidovabdurashid572@gmail.com>
2025-10-22 14:18:39 +0500

    6-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,57 @@
 // from react imports
-import { useState } from "react";
+import { useState } from 'react'
+
 
 // rrd imports
-import { Routes, Route } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 
 // style
-import "./App.scss";
+import './App.scss'
 
 // hooks
-import { UseFetch } from "./Hooks/UseFetch/UseFetch";
+import { UseFetch } from './Hooks/UseFetch/UseFetch'
+
 
 // pages
-import TotalBase from "./Pages/TotalBase/TotalBase";
-import DailyTrade from "./Pages/DailyTrade/DailyTrade";
-import DailyLadger from "./Pages/DailyLadger/DailyLadger";
+import TotalBase from "./Pages/TotalBase/TotalBase"
+import DailyTrade from "./Pages/DailyTrade/DailyTrade"
+import DailyLadger from "./Pages/DailyLadger/DailyLadger"
+
 
 // Components
-import CreatingProduct from "./Components/CreatingProduct/CreatingProduct";
-import ProductList from "./Components/ProductList/ProductList";
+import CreatingProduct from "./Components/CreatingProduct/CreatingProduct" 
+import ProductList from "./Components/ProductList/ProductList" 
 
 // layOuts
-import RootLayOut from "./LayOuts/RootLayOut/RootLayOut";
+import RootLayOut from "./LayOuts/RootLayOut/RootLayOut"
 
 function App() {
-  const [url, setUrl] = useState("http://localhost:3000/products");
-  const { data } = UseFetch(url);
 
-  return (
-    <Routes>
-      <Route element={<RootLayOut />}>
-        <Route path="/TotalBase" element={<TotalBase />}>
-          <Route path="CreatingProduct" element={<CreatingProduct />} />
-          <Route index element={<ProductList />} />
+  const [url, setUrl] = useState("http://localhost:3000/products")
+  const {data} = UseFetch(url)
+
+
+  const routes = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<RootLayOut/>} >
+        <Route path='/TotalBase' element={<TotalBase/>} >
+          <Route path='CreatingProduct' element={<CreatingProduct/>} />
+          <Route index element={<ProductList/>} />
         </Route>
 
-        <Route path="/" element={<DailyTrade />} />
-        <Route path="/DailyLadger" element={<DailyLadger />} />
+        <Route path='/' element={<DailyTrade/>}/>
+        <Route path='/DailyLadger' element={<DailyLadger/>} />
       </Route>
-    </Routes>
-  );
+    )
+  )
+  
+
+
+  return (
+    <>
+    <RouterProvider router={routes} />
+    </>
+  )
 }
 
-export default App;
+export default App
